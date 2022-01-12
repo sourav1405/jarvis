b38921f705d9e42e86a599763e8bde1cebb1c80f
sourav1405 <73256458+sourav1405@users.noreply.github.com>
2022-01-12 11:44:54 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
 
 
         elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+            music_dir = 'c:\\\\songs\\Favorite Songs2'
             songs = os.listdir(music_dir)
             print(songs)
             os.startfile(os.path.join(music_dir, songs[0]))
@@ -94,7 +94,7 @@ if __name__ == "__main__":
             speak(f"Sir, the time is {strTime}")
 
         elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            codePath = "C:\\Users\\Sourav\\MyComputer\\Local\\Programs\\Microsoft VS Code\\Code.exe"
             os.startfile(codePath)
 
         elif 'email to sourav' in query:
